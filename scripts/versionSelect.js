7db99c4f5f3c085330f0de84253ec3ce8ebc5c58
Andrew McCallum <amccallum89@gmail.com>
2022-03-26 13:41:51 +0000

    Added version select script and pre-commit hook

---
+++ scripts/versionSelect.js
@@ -0,0 +1,26 @@
+'use strict';
+const { exec } = require('child_process');
+const { promisify } = require('util')
+const inquirer = require('inquirer');
+
+const asyncExec = promisify(exec);
+
+const question = {
+  type: 'list',
+  name: 'version',
+  message: 'Semantic Version?',
+  choices: ['Patch', 'Minor', 'Major'],
+  filter(val) {
+    return val.toLowerCase();
+  },
+};
+
+(async () => {
+  // const { version } = await inquirer.prompt(question);
+  // await asyncExec(`npm version ${version} --no-git-tag-version`);
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, 10000)
+  })
+})();
